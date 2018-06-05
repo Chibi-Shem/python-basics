2292f7cbe4015eb226f5a13597e7316624739600
Shem <carl.ipanag@gmail.com>
2018-06-05 23:52:11 +0800

    Complete Scripts

--- FilesIO.py
+++ FilesIO.py
@@ -1,16 +1,22 @@
+import os
+
 print('hello python')
 x=raw_input('input something: ')
 print('you typed: '+x)
 x=input('input an expression: ')
 print('the answer is: '+str(x))
-myfile=open('file','a')
+myfile=open('testfile','a')
 print(myfile.name)
 print(myfile.mode)
 print(myfile.closed)
 myfile.write('!!!\n')
 myfile.close()
-myfile=open('file','r')
+myfile=open('testfile','r')
 for word in myfile.readlines():
 	print(word)
-print(myfile.read(0))
+myfile.seek(0,0)
+print(myfile.read(10))
 print(myfile.tell())
+myfile.close()
+
+print(os.getcwd())
